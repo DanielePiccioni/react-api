c48a93232e44da646e3d854fd7336fc626a6ab13
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-12 13:00:17 +0100

    call axios api and stamp the array in console

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,21 @@ function App() {
   /* salvare il link api dentro una constante */
   const API = "https://lanciweb.github.io/demo/api/actors/"
 
+  useEffect(() => {
+    /* call azios */
+    axios
+      .get(API)
+      .then((risposta) => {
+        setAttori(risposta.data);
+        console.log("Lista attori:", risposta.data);
+      })
+      .catch((errore) => {
+        console.error("Errore:", errore);
+      });
+  }, []);
   return (
     <>
-
+      <h1>meowattori</h1>
     </>
   )
 }
