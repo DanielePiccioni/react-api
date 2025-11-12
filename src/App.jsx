83020f62ccc205661313bf51b8b822318f2ef5cb
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-12 12:18:35 +0100

    install axios and save 2 const . one to save the list of actor and other to save link for better call axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
+import { useEffect, useState } from "react"
+import axios from "axios"
 
+
+function App() {
+  /* salvare lista attori */
+  const [attori, setAttori] = useState([]);
+  /* salvare il link api dentro una constante */
+  const API = "https://lanciweb.github.io/demo/api/actors/"
 
   return (
     <>
